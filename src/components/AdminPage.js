0d64029d7ab9acbb2368e84b2812e488b24a4978
James Hartig <fastest963@gmail.com>
2019-03-30 11:39:15 -0400

    Added login around admin routes

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { AdminCompanyPage } from './AdminCompanyPage';
 import { AdminIndex } from './AdminIndex';
+import { AdminRoute } from './AdminRoute';
+import { AdminLogin } from './AdminLogin';
 
 export const AdminPage = () => (
   <>
-    <Route exact path="/admin" component={AdminIndex} />
-    <Route
+    <AdminRoute exact path="/admin" component={AdminIndex} />
+    <Route exact path="/admin/login" component={AdminLogin} />
+    <AdminRoute
       exact
       path="/admin/companies/:company"
       component={AdminCompanyPage}
