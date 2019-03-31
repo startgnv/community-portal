57cf58ff69d693327f2458141b8c522da260e583
William Richardson <will@getadmiral.com>
2019-03-31 16:12:50 -0400

    added custom map pin

--- src/components/MapPin.js
+++ src/components/MapPin.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import mapPin from '../assets/images/map-pin.svg';
 
 const PinContainer = styled.svg`
   transform: translate(${({ height }) => `${-height / 2}px, ${-height / 2}px`});
 `;
 
 export const MapPin = ({ size = 10, active }) => (
-  <PinContainer height={active ? size * 2 : size} viewBox="0 0 24 24">
-    <circle
-      fill={active ? 'blue' : 'black'}
-      r={active ? size : size / 2}
-      cx={active ? size : size / 2}
-      cy={active ? size : size / 2}
-    />
-  </PinContainer>
+  <img height={active ? size * 2 : size} src={mapPin} alt="pin" />
 );
 
 export default MapPin;
