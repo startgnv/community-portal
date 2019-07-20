5d55a980500a437d6f497177904d316ae9bc0cbb
James Hartig <fastest963@gmail.com>
2019-07-20 11:27:48 -0400

    Added firebase files and use implicit firebase importing

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
-
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/storage';
+/*
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE,
   authDomain: 'startupgnv-39bca.firebaseapp.com',
@@ -13,8 +13,18 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+// initialize firebase
+const s = document.createElement('script');
+s.type = 'text/javascript';
+s.src = '/__/firebase/init.js';
+document.head.appendChild(s);
+*/
+
+const firebase = window.firebase;
+
 export const db = firebase.firestore();
-db.enablePersistence();
+db.enablePersistence(); // enables offline data
 export const auth = firebase.auth();
 export const storage = firebase.storage();
 export default firebase;
