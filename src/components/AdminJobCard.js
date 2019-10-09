eeba90f7ca8ae93f82d567f280b8b16a28ed986a
William Richardson <will@getadmiral.com>
2019-10-08 22:26:02 -0400

    added job deletion

--- src/components/AdminJobCard.js
+++ src/components/AdminJobCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDocumentDataOnce } from 'react-firebase-hooks/firestore';
 import { Link } from 'react-router-dom';
 import { useDownloadURL } from 'react-firebase-hooks/storage';
@@ -11,6 +11,13 @@ import {
   IconButton
 } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import { db, storage } from '../firebase';
 import { useAdminContainer } from './AdminPageContainer';
@@ -56,6 +63,9 @@ const AdminJobCard = ({
     job && storage.ref(`companyCovers/${job.companyID}`)
   );
 
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+
   useAdminContainer({
     loading: loading || loadingCompany,
     backTo: '/admin/jobs'
@@ -69,28 +79,85 @@ const AdminJobCard = ({
     return 'Job does not exist';
   }
 
-  return (
-    <Card className={classes.jobCard}>
-      <CardMedia className={classes.jobMedia} image={coverImgURL} />
-      <CardHeader
-        title={job.title}
-        subheader={
-          <AdminJobSubheader
-            category={job.category}
-            company={company ? company.name : undefined}
-            salary={job.salary}
-            applyURL={job.applyURL}
-          />
-        }
-        action={
-          <IconButton component={Link} to={`/admin/jobs/${jobID}/edit`}>
-            <EditIcon />
-          </IconButton>
-        }
-      />
+  const onDeleteClick = () => {
+    db.doc(`jobs/${jobID}`)
+      .delete()
+      .then(() => {
+        setConfirmDelete(false);
+        setDeleted(true);
+      })
+      .catch(() => {});
+  };
 
-      <CardContent>{job.description}</CardContent>
-    </Card>
+  const onUndeleteClick = () => {
+    db.doc(`jobs/${jobID}`)
+      .set(job)
+      .then(() => {
+        setDeleted(false);
+      });
+  };
+
+  return (
+    <>
+      <Dialog open={confirmDelete} onClose={() => setConfirmDelete(false)}>
+        <DialogTitle>Delete {job.title}?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            This job will be gone forever. You need to be really really sure you
+            want to delete it.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => setConfirmDelete(false)}
+            color="primary"
+            autoFocus
+          >
+            Cancel
+          </Button>
+          <Button onClick={onDeleteClick} color="primary">
+            Delete Forever
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {deleted && (
+        <Card className={classes.jobCard}>
+          <CardContent>
+            <p>This job has been deleted.</p>
+            <Button onClick={onUndeleteClick}>Undelete</Button>
+          </CardContent>
+        </Card>
+      )}
+      <Card className={classes.jobCard}>
+        <CardMedia className={classes.jobMedia} image={coverImgURL} />
+        <CardHeader
+          title={job.title}
+          subheader={
+            <AdminJobSubheader
+              category={job.category}
+              company={company ? company.name : undefined}
+              salary={job.salary}
+              applyURL={job.applyURL}
+            />
+          }
+          action={
+            <>
+              <IconButton
+                component={Link}
+                onClick={() => setConfirmDelete(true)}
+              >
+                <DeleteIcon />
+              </IconButton>
+              <IconButton component={Link} to={`/admin/jobs/${jobID}/edit`}>
+                <EditIcon />
+              </IconButton>
+            </>
+          }
+        />
+
+        <CardContent>{job.description}</CardContent>
+      </Card>
+    </>
   );
 };
 
