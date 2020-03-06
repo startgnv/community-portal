f659f59c32788caa8e625d3da2115ff6aedf6b72
Jacob Bleser <jacobbleser@aol.com>
2020-03-06 13:04:48 -0500

    Added newsletter functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
+import { validate } from 'email-validator';
 import { device } from './device';
+import { db } from '../firebase';
 import { clearFix } from 'polished';
 import Logo from './Logo';
 
@@ -99,6 +101,24 @@ const EmailForm = styled.div`
   }
 `;
 
+const EmailErrorLabel = styled.label`
+  color: #ff3333;
+  font-size: 10px;
+  font-family: Arial, sans-serif;
+  margin-top: 20px;
+  margin-bottom: 3px;
+  align-self: flex-start;
+`;
+
+const EmailSubmitLabel = styled.label`
+  color: #00e171;
+  font-size: 10px;
+  font-family: Arial, sans-serif;
+  margin-top: 20px;
+  margin-bottom: 3px;
+  align-self: flex-start;
+`;
+
 const EmailInput = styled.input`
   box-sizing: border-box;
   background: none;
@@ -115,6 +135,19 @@ const EmailInput = styled.input`
   &::placeholder {
     color: rgba(${({ theme }) => theme.lightBlue}, 0.8);
   }
+
+  ${({ error }) =>
+    error &&
+    `
+    border: 1px solid #ff3333;
+    margin-top: 0;
+  `}
+  ${({ submitted }) =>
+    submitted &&
+    `
+    border: 1px solid #00e171;
+    margin-top: 0;
+  `}
 `;
 
 const EmailSubmit = styled.button`
@@ -133,66 +166,105 @@ const EmailSubmit = styled.button`
   padding: 12px 0;
 `;
 
-const Footer = () => (
-  <FooterContainer>
-    <FooterRow>
-      <FooterColumn>
-        <Info>
-          <Logo />
-          <Description>
-            startGNV is an initiative by startupGNV to promote and grow the
-            Gainesville startup, tech, and biotech communities.
-          </Description>
-        </Info>
-        <FooterNav>
-          <FooterHeader>Explore startGNV</FooterHeader>
-          <NavList>
-            <li>
-              <Link to="/companies" class="footer-link">
-                Companies
-              </Link>
-            </li>
-            <li>
-              <Link to="/jobs" class="footer-link">
-                Jobs
-              </Link>
-            </li>
-            <li>
-              <Link to="/new-to-gainesville" class="footer-link">
-                New to Gainesville?
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" class="footer-link">
-                About
-              </Link>
-            </li>
-          </NavList>
-        </FooterNav>
-        <Contact>
-          <FooterHeader>Contact</FooterHeader>
-          <NavList>
-            <li>
-              <a href="mailto:info@startupgnv.com" className="footer-link">
-                info@startupgnv.com
-              </a>
-            </li>
-          </NavList>
-        </Contact>
-      </FooterColumn>
-      <FooterColumn>
-        <EmailForm>
-          <FooterHeader>SUBSCRIBE TO OUR NEWSLETTER</FooterHeader>
-          <EmailInput placeholder="Email" />
-          <EmailSubmit>SUBMIT</EmailSubmit>
-          <Description>
-            Stay notified for regular updates on upcoming events and ways to get
-            involved
-          </Description>
-        </EmailForm>
-      </FooterColumn>
-    </FooterRow>
-  </FooterContainer>
-);
+const Footer = () => {
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const submitInput = async () => {
+    if (!validate(input)) {
+      setError('Please enter a valid email address');
+    } else {
+      try {
+        await db
+          .collection('newsletterSubscribers')
+          .doc(input)
+          .set({});
+        console.log('Document Created!');
+        setInput('');
+        setError('');
+        setSubmitted(true);
+      } catch (err) {
+        console.error('Error creating document: ', err);
+        setError('Unable to submit, please try again later.');
+        setInput('');
+      }
+    }
+  };
+
+  return (
+    <FooterContainer>
+      <FooterRow>
+        <FooterColumn>
+          <Info>
+            <Logo />
+            <Description>
+              startGNV is an initiative by startupGNV to promote and grow the
+              Gainesville startup, tech, and biotech communities.
+            </Description>
+          </Info>
+          <FooterNav>
+            <FooterHeader>Explore startGNV</FooterHeader>
+            <NavList>
+              <li>
+                <Link to="/companies" class="footer-link">
+                  Companies
+                </Link>
+              </li>
+              <li>
+                <Link to="/jobs" class="footer-link">
+                  Jobs
+                </Link>
+              </li>
+              <li>
+                <Link to="/new-to-gainesville" class="footer-link">
+                  New to Gainesville?
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" class="footer-link">
+                  About
+                </Link>
+              </li>
+            </NavList>
+          </FooterNav>
+          <Contact>
+            <FooterHeader>Contact</FooterHeader>
+            <NavList>
+              <li>
+                <a href="mailto:info@startupgnv.com" className="footer-link">
+                  info@startupgnv.com
+                </a>
+              </li>
+            </NavList>
+          </Contact>
+        </FooterColumn>
+        <FooterColumn>
+          <EmailForm>
+            <FooterHeader>SUBSCRIBE TO OUR NEWSLETTER</FooterHeader>
+            {error && <EmailErrorLabel>{error}</EmailErrorLabel>}
+            {submitted && (
+              <EmailSubmitLabel>Thank you for signing up!</EmailSubmitLabel>
+            )}
+            <EmailInput
+              disabled={submitted}
+              submitted={submitted}
+              error={error}
+              placeholder="Email"
+              type="text"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+            />
+            <EmailSubmit onClick={submitInput}>SUBMIT</EmailSubmit>
+            <Description>
+              Stay notified for regular updates on upcoming events and ways to
+              get involved
+            </Description>
+          </EmailForm>
+        </FooterColumn>
+      </FooterRow>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
