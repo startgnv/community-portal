a44675d480dbb36181eb7075c8f6ae67bc376ea7
Jacob Bleser <jacob@studioreach.io>
2020-06-30 12:37:51 -0400

    Added prerendering function for blog meta tags

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
-import {emailCompanyResponse} from "./emailCompanyResponse";
-import {addNewsletterSubscriber} from "./addNewsletterSubscriber";
+import { emailCompanyResponse } from './emailCompanyResponse'
+import { addNewsletterSubscriber } from './addNewsletterSubscriber'
+import { prerenderArticle } from './prerenderArticle'
 
 
-exports.emailCompanyResponse = emailCompanyResponse;
-exports.addNewsletterSubscriber = addNewsletterSubscriber;
+exports.emailCompanyResponse = emailCompanyResponse
+exports.addNewsletterSubscriber = addNewsletterSubscriber
+exports.prerenderArticle = prerenderArticle
