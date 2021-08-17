200703bd37b9a88fe6730f749f5f10cdbc2928c1
seyi oluwaleimu <s.oluwaleimu@gmail.com>
2021-08-17 17:46:54 -0400

    refactoring head content

--- pages/_document.js
+++ pages/_document.js
@@ -33,42 +33,62 @@ export default class MyDocument extends Document {
   render() {
       return(
         <Html lang="en" dir="ltr">
-            <Head>
-              <meta name="application-name" content={APP_NAME} />
-              <meta name="description" content={APP_DESCRIPTION} />
-              <meta name="format-detection" content="telephone=no" />
-              <meta name="mobile-web-app-capable" content="yes" />
-              <meta name="theme-color" content="#000000" />
-              
-              <meta name="apple-mobile-web-app-capable" content="yes" />
-              <meta name="apple-touch-fullscreen" content="yes" />
-              <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-              <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <Head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
+            <link rel="stylesheet" href="https://use.typekit.net/urt4ozf.css"/>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+            <meta
+              name="og:title"
+              property="og:title"
+              content="startGNV - Gainesville's Startup, Tech, and Biotech Community"
+            />
+            <meta
+              name="og:description"
+              property="og:description"
+              content="startGNV is an initiative by startupGNV to promote and grow the Gainesville startup, tech, and biotech communities."
+            />
+            <meta name="og:image" property="og:image" content="https://firebasestorage.googleapis.com/v0/b/startupgnv-39bca.appspot.com/o/home-hero.jpg?alt=media&token=29ee21af-5f63-4bb6-8198-dd29d48769f2" />
+            <meta property="og:type" content="website" />
+            <title>startGNV - Gainesville's Startup, Tech, and Biotech Community</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="theme-color" content="#ffffff"/>
+            <script src="/__/firebase/8.2.6/firebase-app.js"></script>
+            <script src="/__/firebase/8.2.6/firebase-auth.js"></script>
+            <script src="/__/firebase/8.2.6/firebase-firestore.js"></script>
+            <script src="/__/firebase/8.2.6/firebase-storage.js"></script>
+            <script src="/__/firebase/8.2.6/firebase-functions.js"></script>
+            <script src="/__/firebase/init.js"></script>
+¸
+            <script src="static/facebook.js"></script>
+            <noscript>
+              <img height="1" width="1"
+            src="https://www.facebook.com/tr?id=2699241636756946&ev=PageView
+            &noscript=1"/>
+            </noscript>
+            {/* <!-- End Facebook Pixel Code --> */}
 
-              {/* TODO: THESE ARE TEMPORARY, REMOVE WHEN A MORE PERMANENT SOLUTION FOR OPEN GRAPH TAGS IS IN PLACE */}
-              <meta
-                  name="og:title"
-                  property="og:title"
-                  content="startGNV - Gainesville's Startup, Tech, and Biotech Community"
-              />
-              <meta
-                  name="og:description"
-                  property="og:description"
-                  content="startGNV is an initiative by startupGNV to promote and grow the Gainesville startup, tech, and biotech communities."
-              />
-              <meta name="og:image" property="og:image" content="https://firebasestorage.googleapis.com/v0/b/startupgnv-39bca.appspot.com/o/home-hero.jpg?alt=media&token=29ee21af-5f63-4bb6-8198-dd29d48769f2" />
-              <meta property="og:type" content="website" />
-              
-              <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
-              <link rel="stylesheet" href="https://use.typekit.net/urt4ozf.css" />
-              <link rel="manifest" href="/manifest.json" />
-              <link rel="shortcut icon" href="/favicon.ico" />
+            {/* <!-- Mailchimp --> */}
+            <script id="mcjs" src = "/static/mailchimp"></script>
+            {/* <!-- End Mailchimp --> */}
 
-            </Head>
-            <body>
-              <Main />
-              <NextScript />
-            </body>
+            {/* <!-- Hotjar Tracking Code for startgnv.com --> */}
+            <script src="/static/hotjar.js"></script>
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
         </Html>
       )
   }
---
+++ static/mailchimp.js
@@ -0,0 +1,1 @@
+export default !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/7be7ab2c0b7d8d9ef3594572b/5b07ae6f835ce6b3c4fb987d9.js");
