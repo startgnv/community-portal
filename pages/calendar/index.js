398c4f9920800f6a2f682bcf6781f11a4bfbb68b
Bandy <pemisin.toyo@gmail.com>
2021-09-24 11:42:02 -0400

    calendar skeleton added

---
+++ pages/calendar/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// npm i react-embedded-google-calendar
+import { ReactEmbeddedGoogleCalendar } from 'react-embedded-google-calendar';
+const Calendar = () => <ReactEmbeddedGoogleCalendar publicUrl="https://calendar.google.com/calendar/embed?src=pemisin.toyo%40gmail.com&ctz=America%2FNew_York" height= '400px'/>
+
+export default Calendar;
+
+
