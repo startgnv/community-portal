3690a48a177b7107ab7b86b331be78f338602bfe
Bandy <pemisin.toyo@gmail.com>
2021-11-27 13:01:54 -0500

    view error resolved

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -4,42 +4,64 @@ import moment from 'moment'
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import { getEvents } from './gcal'
 
-import { Calendar, dateFnsLocalizer} from 'react-big-calendar'
+import { Calendar, momentLocalizer} from 'react-big-calendar'
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
 
-const locales = {
-  'en-US': enUS,
+// Add padding to the calendar and altering the colors and font to match the rest of the site.
+const eventStyleGetter = (event, start, end, isSelected) =>{
+  let style = {
+      backgroundColor: '#f35b1a',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+      border: '0px',
+      display: 'block',
+      fontSize: '12px',
+      fontFamily: 'Arial,sans-serif',
+      fontWeight: 'bold'
+  };
+  return {
+      style: style
+  };
 }
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+const calendarStyle = {
+  height: '800px',
+  padding: ' 0.5rem 0',
+}
+
+// const ColoredDateCellWrapper = ({ children }) =>
+//   React.cloneElement(React.Children.only(children), {
+//     style: {
+//       color: 'red',
+//     },
+//   })
+
+
+const localizer = momentLocalizer(moment)
 
 const EventCalendar = props => {
   let {eventList} = props;
 
   eventList = eventList.map(event => ({
     ...event,
-    start: JSON.parse(event.start),
-    end: JSON.parse(event.end)
+    start: new Date(JSON.parse(event.start)),
+    end: new Date(JSON.parse(event.end))
   }));
 
   return(
     <div>
         <Calendar
-        style={{height: '420px'}}
+        style={calendarStyle}
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
         events={eventList}
+        eventPropGetter={eventStyleGetter}
         />
     </div>
   )
