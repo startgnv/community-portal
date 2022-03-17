fa18b14247b8c26b65d06e596d1db3213ca833d9
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2022-03-17 12:47:15 -0400

    import casing

--- src/components/Site/Companies/CompaniesList.js
+++ src/components/Site/Companies/CompaniesList.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 import CompanyListItemLarge from './CompanyListItemLarge';
 import { SharedMapConsumer } from './CompaniesMapContext';
 import { device } from '../../utils/device';
-import Link from '../ui/Link';
+import Link from '../UI/Link';
 import Button from '../UI/Button';
 
 const Container = styled.div`
